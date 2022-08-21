d631d806940d46e413d231da7ff9cbb814937938
Mahdi Bahrami <github@mahdibm.com>
2022-08-22 02:37:28 +0430

    added Procfile

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,11 +2,13 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    // vapordemo.com
     app.get { req async throws in
         try await req.view.render("index", ["title": "Hello Vapor!"])
     }
-
-    app.get("hello") { req async -> String in
+    
+    // vapordemo.com/hello
+    app.get("hello") { req -> String in
         "Hello, world!"
     }
 
