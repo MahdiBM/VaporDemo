f52036bb781bd61824d21fdbf46e7cfdf847f3b4
Mahdi Bahrami <github@mahdibm.com>
2022-08-26 03:50:48 +0430

    test push

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,6 +19,8 @@ public func configure(_ app: Application) throws {
 
     app.migrations.add(CreateTodo())
     
+//    try app.autoMigrate().wait()
+    
     app.views.use(.leaf)
     
     // register routes
