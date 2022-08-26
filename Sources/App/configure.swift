36c43143fe87ed0fa7e2cefd24e289da0b3a050e
Mahdi Bahrami <github@mahdibm.com>
2022-08-27 03:14:04 +0430

    trying release phase

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,8 +19,6 @@ public func configure(_ app: Application) throws {
 
     app.migrations.add(CreateTodo())
     
-    try app.autoMigrate().wait()
-    
     app.views.use(.leaf)
     
     // register routes
